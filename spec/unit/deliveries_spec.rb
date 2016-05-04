85ae16e40c67f0d1fafe5eb2ce1bb826e819b913
Simon Glancy <simonglancy@Simons-MacBook-Pro-2.local>
2016-05-04 13:57:39 +0100

    data structure updated

--- spec/unit/deliveries_spec.rb
+++ spec/unit/deliveries_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 describe DeliveriesController, 'testing deliveries' do
-  let(:delivery) { create :delivery, sender_name: 'Phil' }
+  before(:each) do
+    @delivery = create :delivery
+  end
 
   it 'displays existing delivery requests', type: :request do
     get '/'
@@ -16,26 +18,26 @@ describe DeliveriesController, 'testing deliveries' do
   end
 
   it 'allows user to create delivery request', type: :request do
-    params = build_params
+    params = attributes_for(:delivery, recipient_name: "Andrew")
     post '/deliveries.json', delivery: params
     expect(Delivery.last.recipient_name).to eq('Andrew')
   end
 
   it 'allows user to change status to assigned', type: :request do
     params = {status: 'assigned'}
-    patch "/deliveries/#{delivery.id}.json", delivery: params
+    patch "/deliveries/#{@delivery.id}.json", delivery: params
     expect(Delivery.last.status).to eq('assigned')
   end
 
   it 'allows user to change status to collected', type: :request do
     params = {status: 'collected'}
-    patch "/deliveries/#{delivery.id}.json", delivery: params
+    patch "/deliveries/#{@delivery.id}.json", delivery: params
     expect(Delivery.last.status).to eq('collected')
   end
 
   it 'allows user to change status to delivered', type: :request do
     params = {status: 'delivered'}
-    patch "/deliveries/#{delivery.id}.json", delivery: params
+    patch "/deliveries/#{@delivery.id}.json", delivery: params
     expect(Delivery.last.status).to eq('delivered')
   end
 end
