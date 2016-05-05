0687c8197913526b66d35ecaf6e16b607ad83b41
Andrew Cookson <acookson91@gmail.com>
2016-05-05 20:01:17 +0100

    allowed put request

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module HandlBackend
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :patch]
       end
     end
 
