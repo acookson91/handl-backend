7b80c136248612b7d5a6bbb79f3010cabd0748f5
Gabrielle <gabivk@gmail.com>
2016-05-11 17:31:52 +0100

    added parameter permissions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters
 
     protect_from_forgery with: :null_session
     respond_to :json
 
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
