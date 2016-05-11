c1f33b707d28c5901b3390d9cc073ab4614cae7b
Simon Glancy <simonglancy@Simons-MacBook-Pro-6.local>
2016-05-11 12:05:26 +0100

    added cors config

--- config/application.rb
+++ config/application.rb
@@ -32,12 +32,23 @@ module HandlBackend
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+
+
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :patch]
-      end
-    end
 
+      resource '/cors',
+        :headers => :any,
+        :methods => [:post],
+        :credentials => true,
+        :max_age => 0
+
+      resource '*',
+        :headers => :any,
+        :methods => [:get, :post, :delete, :put, :patch, :head],
+        :max_age => 0,
+        :expose => ['Access-token','Token-type','expiry','client','uid']
+    end
   end
 end
